85b9d2644d484b8db376dab05378bdfc13df5013
Tech Taurus <71295055+torusbrane@users.noreply.github.com>
2023-06-08 09:07:42 -0500

    Adding the initial iteration of the triangle code.

---
+++ PascalTriangle.py
@@ -0,0 +1,25 @@
+row_count = int(input("Enter a number for the Pascal Triangle: "))
+
+def pascal_triangle():
+    global row_count
+    start_row_count = 0
+    for i in range(row_count):
+        first_num = 1
+        row_count -= 1
+        wht_spc = str(' ' * row_count)
+        start_row_count += 2
+        wht_spc2 = str(start_row_count)
+        new_start_row = start_row_count - 1
+        wht_spc3 = str(' ' * new_start_row)
+        # wht_spc4 = str(new_start_row + first_num)
+        if i < 1:
+            print(wht_spc + str(first_num) + wht_spc2 + str(first_num))
+        else:
+            print(wht_spc + str(first_num) + wht_spc3 + str(first_num))
+        # else:
+        #     print(wht_spc + str(first_num) + wht_spc4 + str(first_num))
+
+init_wht_spc = str(' ' * row_count)
+
+print(init_wht_spc + "1")     
+pascal_triangle()
