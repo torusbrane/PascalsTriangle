93c29a4a657193431cafd420dfdac1cdb4c2053a
Tech Taurus <71295055+torusbrane@users.noreply.github.com>
2023-06-08 09:43:31 -0500

    Used 'wht_spc_4' instead of 'wht_spc_3'

--- PascalTriangle.py
+++ PascalTriangle.py
@@ -10,16 +10,18 @@ def pascal_triangle():
         start_row_count += 2
         wht_spc2 = str(start_row_count)
         new_start_row = start_row_count - 1
-        wht_spc3 = str(' ' * new_start_row)
-        # wht_spc4 = str(new_start_row + first_num)
+        # wht_spc3 = str(' ' * new_start_row)
+        wht_spc4 = str(new_start_row + first_num)
         if i < 1:
             print(wht_spc + str(first_num) + wht_spc2 + str(first_num))
-        else:
-            print(wht_spc + str(first_num) + wht_spc3 + str(first_num))
         # else:
-        #     print(wht_spc + str(first_num) + wht_spc4 + str(first_num))
+        #     print(wht_spc + str(first_num) + wht_spc3 + str(first_num))
+        else:
+            print(wht_spc + str(first_num) + wht_spc4 + str(first_num))
 
 init_wht_spc = str(' ' * row_count)
 
 print(init_wht_spc + "1")     
 pascal_triangle()
+
+#ADDED WHITE SPACE 4 AND USED THAT INSTEAD OF WHITE SPACE 3
